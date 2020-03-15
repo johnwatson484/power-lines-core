b40fcb6fab72dc0fa8ec213547d93ed510799148
John Watson <johnwatson484@hotmail.com>
2020-03-15 14:11:15 +0000

    Volume removal optional

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,20 +3,20 @@ docker-compose \
   -f ../power-lines-data-service/docker-compose.override.yaml \
   -f ../power-lines-data-service/docker-compose.link.yaml \
   down \
-  -v
+  $@
 
 docker-compose \
   -f ../power-lines-fixture-service/docker-compose.yaml \
   -f ../power-lines-fixture-service/docker-compose.override.yaml \
   -f ../power-lines-fixture-service/docker-compose.link.yaml \
   down \
-  -v
+  $@
 
 docker-compose \
   -f ../power-lines-analysis-service/docker-compose.yaml \
   -f ../power-lines-analysis-service/docker-compose.override.yaml \
   -f ../power-lines-analysis-service/docker-compose.link.yaml \
   down \
-  -v
+  $@
 
 docker network rm power-lines
